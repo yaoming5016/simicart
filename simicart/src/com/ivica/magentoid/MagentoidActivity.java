89e4873c68ecd558d3a6268668b4374107e5e8f0
Clessmax <Clessmax@192.168.0.102>
2015-05-25 23:47:59 +0000

    add menu

--- simicart/src/com/ivica/magentoid/MagentoidActivity.java
+++ simicart/src/com/ivica/magentoid/MagentoidActivity.java
@@ -5,21 +5,30 @@
  */
 package com.ivica.magentoid;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.ivica.magentoid.core.MagentoidApp;
 
-public class MagentoidActivity extends Activity {
-
-	MagentoidApp mApp = MagentoidApp.getInstance();
+public class MagentoidActivity extends FragmentActivity {
+	private android.support.v4.widget.DrawerLayout drawerLayout;
+	private ListView left_drawer;
+	private MagentoidApp mApp = MagentoidApp.getInstance();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		left_drawer = (ListView) findViewById(R.id.left_drawer);
+		setupMenuLeft();
 		if (mApp.getConfiguration().ds == null) {
 			Toast.makeText(this, "Nothing", Toast.LENGTH_LONG).show();
 		} else {
@@ -33,4 +42,29 @@ public class MagentoidActivity extends Activity {
 		}
 	}
 
+	private void setupMenuLeft() {
+		String[] datas = new String[] {//
+		getResources().getString(R.string.home),//
+				getResources().getString(R.string.category),//
+				getResources().getString(R.string.profile),//
+				getResources().getString(R.string.addressbook),//
+				getResources().getString(R.string.order_history),//
+				getResources().getString(R.string.store_locator),//
+				getResources().getString(R.string.abountus),//
+				getResources().getString(R.string.privacy_policy) //
+		};//
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, datas);
+		left_drawer.setAdapter(adapter);
+		left_drawer.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+			}
+
+		});
+	}
 }
